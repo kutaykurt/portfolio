c7098d7238daafcee4bc3c5853a49e8720d84f51
kutaykurt <kutay_kurt@gmx.de>
2026-02-04 15:09:23 +0100

    Exact recreation of DilYuvam logo from reference image

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -136,10 +136,13 @@ const LogoRenderer = ({ type }) => {
                 <div className="custom-logo dilyuvam-logo">
                     <div className="logo-icon">
                         <svg viewBox="0 0 24 24" fill="currentColor">
-                            <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 14H5.17L4 17.17V4h15v12zM7 9h2v2H7V9zm3 0h8v2h-8V9zm-3-3h11v2H7V6z" />
+                            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                         </svg>
                     </div>
-                    <span className="logo-text">Dil<span>Yuvam</span></span>
+                    <div className="logo-content">
+                        <span className="logo-text">DilYuvam</span>
+                        <div className="logo-bar"></div>
+                    </div>
                 </div>
             );
         case 'admindashboard':
